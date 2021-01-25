e1bed14eae2f3d799efa9d4b6b7acf99700bd8fc
Annikapati Harish <harish2012.2012@gmail.com>
2021-01-25 14:11:02 +0530

    unit3 part2 commit1

---
+++ Unit3/BRRead.java
@@ -0,0 +1,15 @@
+package Unit3;
+import java.io.*;
+class BRRead {
+public static void main(String args[]) throws IOException
+{
+char c;
+BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+System.out.println("Enter characters, 'q' to quit.");
+// read characters
+do {
+c = (char) br.read();
+System.out.println(c);
+} while(c != 'q');
+}
+}
---
+++ Unit3/WriteDemo.java
@@ -0,0 +1,10 @@
+package Unit3;
+
+class WriteDemo {
+public static void main(String args[]) {
+int b;
+b = 65;//65
+System.out.print(b);//print
+System.out.write('\n');
+}
+}
